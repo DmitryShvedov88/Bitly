d52fabd50398ebc87dc40ef306d98feb80d8c513
DmitryShvedov88 <95371870+DmitryShvedov88@users.noreply.github.com>
2023-04-09 20:16:04 +0300

    fixed PEP8 errors

--- URL_shortening.py
+++ URL_shortening.py
@@ -2,23 +2,26 @@ import requests
 import os
 import argparse
 
+
 def shorten_link(user_link):
-    headers={"Authorization": os.getenv("BITLY_TOKEN")}
-    params={"long_url": user_link}
+    headers = {"Authorization": os.getenv("BITLY_TOKEN")}
+    params = {"long_url": user_link}
     short_link = 'https://api-ssl.bitly.com/v4/bitlinks'
     response = requests.post(short_link, json=params, headers=headers)
     response.raise_for_status()
 
     return response.json()["id"]
 
+
 def total_clicks(user_link):
     headers = {"Authorization": os.getenv("BITLY_TOKEN")}
     summ_link = 'https://api-ssl.bitly.com/v4/bitlinks/{bitlink}/clicks/summary'
-    total_clicks = summ_link.format(bitlink=user_link)
-    response = requests.get(total_clicks, headers=headers)
+    total_clicks_repl = summ_link.format(bitlink=user_link)
+    response = requests.get(total_clicks_repl, headers=headers)
     response.raise_for_status()
 
     return response.json()["total_clicks"]
+
 
 def is_bitlink(user_link):
     headers = {"Authorization": os.getenv("BITLY_TOKEN")}
@@ -28,6 +31,7 @@ def is_bitlink(user_link):
     it_is_bitlink = response.ok
 
     return it_is_bitlink
+
 
 def main():
     from dotenv import load_dotenv, find_dotenv
@@ -39,12 +43,11 @@ def main():
     name_url = parser.parse_args()
     user_link = format(name_url.URL)
 
-    if is_bitlink(user_link)==True:
+    if is_bitlink(user_link):
         print('Колличество переходов по ссылки Битли:', total_clicks(user_link))
     else:
         print(f'http://{shorten_link(user_link)}')
 
+
 if __name__ == "__main__":
     main()
-
-

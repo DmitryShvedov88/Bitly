8323f7623d8ff5697cadf896c33f7f9ef61bcfb7
DmitryShvedov88 <95371870+DmitryShvedov88@users.noreply.github.com>
2023-04-14 20:58:55 +0300

    added the ability to use a different protocol

--- URL_shortening.py
+++ URL_shortening.py
@@ -12,18 +12,18 @@ def shorten_link(headers, user_link):
     return response.json()["id"]
 
 
-def total_clicks(headers, user_link):
+def total_clicks(headers, user_link_short):
     summ_link = 'https://api-ssl.bitly.com/v4/bitlinks/{bitlink}/clicks/summary'
-    total_clicks_repl = summ_link.format(bitlink=user_link)
+    total_clicks_repl = summ_link.format(bitlink=user_link_short)
     response = requests.get(total_clicks_repl, headers=headers)
     response.raise_for_status()
 
     return response.json()["total_clicks"]
 
 
-def is_bitlink(headers, user_link):
+def is_bitlink(headers, user_link_short):
     cheak_link = 'https://api-ssl.bitly.com/v4/bitlinks/{bitlink}'
-    cheaker = cheak_link.format(bitlink=user_link)
+    cheaker = cheak_link.format(bitlink=user_link_short)
     response = requests.get(cheaker, headers=headers)
     it_is_bitlink = response.ok
 
@@ -41,12 +41,12 @@ def main():
     name_url = parser.parse_args()
     user_link = format(name_url.URL)
     parsed_link=urlparse(user_link)
-    print(parsed_link)
+    user_link_short=parsed_link.netloc+parsed_link.path
 
     headers = {"Authorization": f'Bearer {os.getenv("BITLY_TOKEN")}'}
 
-    if is_bitlink(headers, user_link):
-        print('Колличество переходов по ссылки Битли:', total_clicks(headers, user_link))
+    if is_bitlink(headers, user_link_short):
+        print('Колличество переходов по ссылки Битли:', total_clicks(headers, user_link_short))
     else:
         print(f'{parsed_link.scheme}://{shorten_link(headers, user_link)}')
 
